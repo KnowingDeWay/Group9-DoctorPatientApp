80fece7c2ae799c0159130446043d62360329e52
adibmkhan <37283920+adibmkhan@users.noreply.github.com>
2018-10-04 19:48:05 +1000

    - UI ad working redirecting button for Doctor REgistration

--- app/src/main/java/com/softwareapp/group9/doctorpatientapp/userprofile/ChoiceRegister.java
+++ app/src/main/java/com/softwareapp/group9/doctorpatientapp/userprofile/ChoiceRegister.java
@@ -1,5 +1,6 @@
 package com.softwareapp.group9.doctorpatientapp.userprofile;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,10 +9,12 @@ import android.support.v7.widget.ToolbarWidgetWrapper;
 import android.view.View;
 import android.widget.Button;
 
+import com.softwareapp.group9.doctorpatientapp.DoctorRegister;
 import com.softwareapp.group9.doctorpatientapp.R;
 
 public class ChoiceRegister extends AppCompatActivity {
     private Button button4;
+    private Button button5;
     private Toolbar mToolbar;
 
     @Override
@@ -31,11 +34,28 @@ public class ChoiceRegister extends AppCompatActivity {
             }
         });
 
+
+        button5= (Button) findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDoctorRegister();
+            }
+        });
+
+
+
     }
 
     public void openPatientRegister(){
 
         Intent intent = new Intent(this, PatientRegister.class);
+        startActivity(intent);
+    }
+
+    public void openDoctorRegister(){
+
+        Intent intent = new Intent(this, DoctorRegister.class);
         startActivity(intent);
     }
 
