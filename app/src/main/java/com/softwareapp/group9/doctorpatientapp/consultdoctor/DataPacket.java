d568104e2c07b973cce84cb0b1ed0305c07d9ea2
KnowingDeWay <12886824@student.uts.edu.au>
2018-10-13 00:33:01 +1100

    Progress on Choose Doctor and Completed Data Packet Class

--- app/src/main/java/com/softwareapp/group9/doctorpatientapp/consultdoctor/DataPacket.java
+++ app/src/main/java/com/softwareapp/group9/doctorpatientapp/consultdoctor/DataPacket.java
@@ -7,4 +7,17 @@ public class DataPacket {
     public String description;
     public String mediaReference;
     public String filesReference;
+
+    public DataPacket(){
+
+    }
+
+    public DataPacket(String patientId, String doctorId, String condition, String description, String mediaReference, String filesReference){
+        this.patientId = patientId;
+        this.doctorId = doctorId;
+        this.condition = condition;
+        this.description = description;
+        this.mediaReference = mediaReference;
+        this.filesReference = filesReference;
+    }
 }
